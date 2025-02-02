a71bf259431d96fd49d107e0b72064c920f61e5c
Lora D <lodushanova@gmail.com>
2025-02-01 16:52:20 -0800

    Wrote server and client, both working

---
+++ zmq_client.c
@@ -0,0 +1,28 @@
+//
+// Created by lora on 2/1/25.
+//
+// Hello World client
+#include <stdio.h>
+#include <zmq.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(void) {
+    printf("Connecting to CS361 server...\n");
+    void *context = zmq_ctx_new();
+    void *requester = zmq_socket(context, ZMQ_REQ);
+    zmq_connect(requester, "tcp://localhost:5555");
+
+    int requester_number;
+
+    for(requester_number = 0; requester_number != 10; requester_number++) {
+        char buffer[30];
+        printf("Sending Hello %d...\n", requester_number);
+        zmq_send(requester, "Hello", 5, 0);
+        zmq_recv(requester, buffer, 30, 0);
+        printf("Received This is a message from CS361 %d\n", requester_number);
+    }
+    zmq_close(requester);
+    zmq_ctx_destroy(context);
+    return 0;
+}
--- zmq_server.c
+++ zmq_server.c
@@ -1,6 +1,24 @@
-#include <stdio.h>
+// Hello World Server
 
-int main(void) {
-    printf("Hello, World!\n");
+#include <zmq.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <assert.h>
+
+int main(int argc, char **argv) {
+    // Socket to talk to clients
+    void *context = zmq_ctx_new();
+    void *responder = zmq_socket(context, ZMQ_REP);
+    int rc = zmq_bind(responder, "tcp://localhost:5555");
+    assert(rc == 0);
+
+    while(1) {
+        char buffer [10];
+        zmq_recv(responder, buffer, 10, 0); //enough buffer to handle Hello 0...\0
+        printf("Received Hello\n");
+        sleep(1);       // Do some 'work'
+        zmq_send(responder, "This is a message from CS361", 28, 0);
+    }
     return 0;
 }
